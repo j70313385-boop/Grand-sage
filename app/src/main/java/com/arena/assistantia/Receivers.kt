6cd9d0990529dc025a5da4258d07bfe968013b00
j70313385-boop <j70313385@gmail.com>
2026-08-06 22:18:25 +0000

    feat: Add BroadcastReceivers

---
+++ app/src/main/java/com/arena/assistantia/Receivers.kt
@@ -0,0 +1,13 @@
+package com.arena.assistantia
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class ReminderReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {}
+}
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {}
+}
